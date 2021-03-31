6ed754441856922bd4ff9957a6c9203ee36b3ac9
Ben Davarias <>
2021-03-31 23:10:09 +0100

    Room booking now works

--- src/resourcebooking/ResourceBooking.java
+++ src/resourcebooking/ResourceBooking.java
@@ -1,6 +1,7 @@
 package resourcebooking;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
@@ -8,26 +9,20 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
-
-
 
 public class ResourceBooking {
 
     public static Scanner input = new Scanner(System.in);
-    
+
     public static ArrayList<Catering> cateringRequests = new ArrayList<>();
     public static ArrayList<Equipment> equipmentRequests = new ArrayList<>();
 
     public static void main(String[] args) {
         cateringRequests = Catering.readFile();
-//        view();
         currentTime();
         mainMenu();
-        //do user login
-        //make a 2d array to book slots, use the grid to represent times, 5 time slots per day, 5 different rooms
-        //in 2d array, put an x when a time slot is booked.
-        //Or a morning and afternoon slot that last 3 hours and refreshments can be delivered every hour on the hour.
 
     }
 
@@ -63,51 +58,97 @@ public class ResourceBooking {
 
     }
 
-    public static void roomBook() {
-        String email=emailVerif();
+    public static void roomBook() throws FileNotFoundException, IOException {
+        //System.out.println("hello");
+        String roomDirectory = System.getProperty("user.dir") + "\\Rooms.txt";
+        //System.out.println(roomDirectory);
+        File file = new File(roomDirectory);
+        ArrayList<room> RoomList = new ArrayList<>();
+        Scanner scanFile = new Scanner(file);
+        int count = 0;
+        while (scanFile.hasNextLine()) {
+            String userEmail = scanFile.next();
+            //System.out.println(userEmail);
+            String roomNumber = scanFile.next();
+           // System.out.println(roomNumber);
+            String selectedDay = scanFile.next();
+            //System.out.println(selectedDay);
+            room Room = new room(userEmail, (Integer.parseInt(roomNumber)), selectedDay);
+           // System.out.println(Room);
+            RoomList.add(Room);
+            count++;
+        }
+
+        String email = emailVerif();
         System.out.println("Do you need disability access?");
-        String access=input.next();
+        String access = input.next();
         int roomSelected;
-        access=access.toLowerCase();
-        if (access.equals("y")||access.equals("yes")){
-            roomSelected=4;
+        access = access.toLowerCase();
+        if (access.equals("y") || access.equals("yes")) {
+            roomSelected = 4;
             System.out.println("room 4 selected");
-        }else{
+        } else {
             System.out.println("How many people are you booking for?(max 50)");
-            int peopleCount=input.nextInt();
-            
-            if(peopleCount>2){
-                roomSelected=1;
-            }else{
-                if(2<peopleCount&&peopleCount<5){
-                    roomSelected=2;
-                }else{
-                    if(4<peopleCount&&peopleCount<9){
-                        roomSelected=3;
-                    }else{
-                        if(8<peopleCount&&peopleCount<16){
-                            roomSelected=4;
-                        }else{
-                            roomSelected=5;
+            int peopleCount = input.nextInt();
+
+            if (peopleCount < 3) {
+                roomSelected = 1;
+            } else {
+                if (2 < peopleCount && peopleCount < 5) {
+                    roomSelected = 2;
+                } else {
+                    if (4 < peopleCount && peopleCount < 9) {
+                        roomSelected = 3;
+                    } else {
+                        if (8 < peopleCount && peopleCount < 16) {
+                            roomSelected = 4;
+                        } else {
+                            roomSelected = 5;
                         }
                     }
                 }
             }
-            System.out.println("room "+roomSelected+" selected\n");
+            System.out.println("room " + roomSelected + " selected\n");
             System.out.println("what day would you like the room?\n1: Monday\n2: Tuesday\n3: Wednesday\n4: Thursday\n5: Friday");
-            int daySelect=input.nextInt();
-            switch(daySelect){
+            int daySelect = input.nextInt();
+            String day = "";
+            switch (daySelect) {
                 case 1:
-                    
+                    day = "monday";
+                    break;
+                case 2:
+                    day = "tuesday";
+                    break;
+                case 3:
+                    day = "wednesday";
+                    break;
+                case 4:
+                    day = "thursday";
+                    break;
+                case 5:
+                    day = "friday";
+                    break;
             }
-            
+            //System.out.println(RoomList);
+            room Room = new room(email, roomSelected, day);
+            RoomList.add(Room);
+            //System.out.println(RoomList);
         }
-    }
+        FileWriter myWriter=new FileWriter(file);
+        for(int i=0;i<RoomList.size();i++){
+            String toBeWritten=((RoomList.get(i)).toString());
+            myWriter.write(toBeWritten);
+            if(i<RoomList.size()-1){
+                myWriter.write("\n");
+            }
+        }
+        myWriter.close();
+    }   
 
     public static void reqRefreshments() {
         int roomNumber = roomVerif();
         String email = emailVerif();
-        
+
         System.out.println("Would you like tea?");
         String tea = input.next();
         System.out.println("Would you like coffee?");
@@ -119,12 +160,12 @@ public class ResourceBooking {
         System.out.println("Would you like sandwiches?");
         String sandwich = input.next();
         String requestTime = currentTime();
-        
-        Catering newRequest = new Catering(email,roomNumber, tea, coffee, water, pastry, sandwich, requestTime);cateringRequests.add(newRequest);
+
+        Catering newRequest = new Catering(email, roomNumber, tea, coffee, water, pastry, sandwich, requestTime);
+        cateringRequests.add(newRequest);
         Catering.writeFile(cateringRequests);
-        
+
         System.out.println(cateringRequests);
-        
 
     }
 
@@ -138,11 +179,9 @@ public class ResourceBooking {
         String lineFromFile;
         BufferedReader read = new BufferedReader(new FileReader(roomDirectory));
         //while((lineFromFile = read.read()) != null){
-            
-        //}
-        
-    }
 
+        //}
+    }
 
     public static String emailVerif() {
         boolean valid = true;
@@ -154,8 +193,11 @@ public class ResourceBooking {
             if (email.contains("@")) {
                 if (email.contains(".co.uk") || email.contains(".com") || email.contains(".ac.uk")) {
                     System.out.println("Email valid.");
+                    valid = false;
+                } else {
+                    System.out.println("Email not valid. Please try again.");
                 }
-                valid = false;
+
             } else {
                 System.out.println("Email not valid. Please try again.");
             }
@@ -184,20 +226,15 @@ public class ResourceBooking {
     public static String currentTime() {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
-        String convertDate = formatter.format(date);                
+        String convertDate = formatter.format(date);
         System.out.println(convertDate);
         return convertDate;
     }
 
-    
-    
-    
     public static void view() {
         for (int i = 0; i < cateringRequests.size(); i++) {
             System.out.println(cateringRequests.get(i).toString());
         }
     }
 
-    
-    
 }
--- src/resourcebooking/room.java
+++ src/resourcebooking/room.java
@@ -1,6 +1,49 @@
 package resourcebooking;
-
+import java.util.ArrayList;
 
 public class room {
+
+    protected String email;
+    protected int roomSelected;
+    protected String day;
+
+    public room(String email,int roomSelected,String day) {
+        this.email=email;
+        this.roomSelected=roomSelected;
+        this.day=day;
+    }
+
+   
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRoomSelected(int roomSelected) {
+        this.roomSelected = roomSelected;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoomSelected() {
+        return roomSelected;
+    }
+
+    public String getDay() {
+        return day;
+    }
+ @Override
+    public String toString() {
+        return email + " " + roomSelected + " " + day;
+    }
     
-}
+    }
+
+    
+
